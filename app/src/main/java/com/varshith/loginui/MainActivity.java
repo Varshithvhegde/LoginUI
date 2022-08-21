162fe69f7b47258190d7882da8225fdb1daf8eac
Varshithvhegde <varshithvh@gmail.com>
2022-08-21 14:18:50 +0530

    Initial commit

---
+++ app/src/main/java/com/varshith/loginui/MainActivity.java
@@ -0,0 +1,57 @@
+package com.varshith.loginui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView signin,signup,signin_signup_txt,forgot_password;
+    CircleImageView circleImageView;
+    Button signin_signup_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        signin = findViewById(R.id.signin);
+        signup = findViewById(R.id.signup);
+        signin_signup_txt = findViewById(R.id.signin_signup_txt);
+        forgot_password = findViewById(R.id.forgot_password);
+        circleImageView = findViewById(R.id.circleImageView);
+        signin_signup_btn = findViewById(R.id.signin_signup_btn);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signin.setTextColor(Color.parseColor("#FFFFFF"));
+                signin.setBackgroundColor(Color.parseColor("#FF2729C3"));
+                signup.setTextColor(Color.parseColor("#FF2729C3"));
+                signup.setBackgroundResource(R.drawable.bordershape);
+                circleImageView.setImageResource(R.drawable.sigin_boy_img);
+                signin_signup_txt.setText("Sign In");
+                signin_signup_btn.setText("Sign In");
+                forgot_password.setVisibility(View.VISIBLE);
+            }
+        });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signup.setTextColor(Color.parseColor("#FFFFFF"));
+                signup.setBackgroundColor(Color.parseColor("#FF2729C3"));
+                signin.setTextColor(Color.parseColor("#FF2729C3"));
+                signin.setBackgroundResource(R.drawable.bordershape);
+                circleImageView.setImageResource(R.drawable.sigup_boy_img);
+                signin_signup_txt.setText("Sign Up");
+                signin_signup_btn.setText("Sign Up");
+                forgot_password.setVisibility(View.INVISIBLE);
+
+            }
+        });
+    }
+}
